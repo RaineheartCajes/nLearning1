5cb9976635502081a18c38fa259b55b7ffcda435
JessBelen <belenjaykin@gmail.com>
2024-05-06 15:50:45 +0800

    button
    icon

--- client/src/components/CKContents/Card.jsx
+++ client/src/components/CKContents/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 import DOMPurify from "dompurify";
+import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
 
 function Card({ card, editCard, deleteCard }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -55,7 +56,10 @@ function Card({ card, editCard, deleteCard }) {
 
   return (
     <>
-      <div className="card-container" style={{ width: "600px", height: "400px" }}>
+      <div
+        className="card-container"
+        style={{ width: "600px", height: "400px" }}
+      >
         <h3
           className="card-title"
           dangerouslySetInnerHTML={createMarkup(card.title)}
@@ -63,39 +67,65 @@ function Card({ card, editCard, deleteCard }) {
         {Array.isArray(card.media) && card.media.length ? (
           renderMediaPreview(card.media)
         ) : (
-          <div style={{ margin: '0 auto' }}>No Media Provided</div>
-
+          <div style={{ margin: "0 auto" }}>No Media Provided</div>
         )}
         <div
           className="card-description"
           dangerouslySetInnerHTML={createMarkup(card.description)}
         ></div>
         <div>
-        <button onClick={toggleModal} className="card-button" style={{ marginRight: 10, backgroundColor: "white", color: "black", borderRadius: "3px", padding: "6px 12px", fontSize: "12px" }}>
-  View Details
-</button>
+          <button
+            onClick={toggleModal}
+            className="card-button"
+            style={{
+              marginRight: 10,
+              backgroundColor: "white",
+              color: "black",
+              borderRadius: "3px",
+              padding: "6px 12px",
+              fontSize: "12px",
+            }}
+          >
+            <FaEye style={{ marginRight: 5 }} /> View Details
+          </button>
 
-<button onClick={() => editCard(card.id)} className="card-button card-button-edit" style={{ backgroundColor: "white", color: "black", borderRadius: "3px", padding: "6px 12px", fontSize: "12px" }}>
-  Edit
-</button>
+          <button
+            onClick={() => editCard(card.id)}
+            className="card-button card-button-edit"
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              borderRadius: "3px",
+              padding: "6px 12px",
+              fontSize: "12px",
+            }}
+          >
+            <FaEdit style={{ marginRight: 5 }} /> Edit
+          </button>
 
-<button onClick={() => deleteCard(card.id)} className="card-button" style={{ backgroundColor: "red", color: "#ffffff", borderRadius: "3px", padding: "6px 12px", fontSize: "12px" }}>
-  Delete
-</button>
-
-
-
+          <button
+            onClick={() => deleteCard(card.id)}
+            className="card-button"
+            style={{
+              backgroundColor: "red",
+              color: "black",
+              borderRadius: "3px",
+              padding: "6px 12px",
+              fontSize: "12px",
+            }}
+          >
+            <FaTrash style={{ marginRight: 5 }} /> Delete
+          </button>
         </div>
       </div>
-
       <Modal isOpen={isModalOpen} onClose={toggleModal}>
-        <h2 dangerouslySetInnerHTML={createMarkup(card.title)}></h2>
-        {Array.isArray(card.media) && card.media.length ? (
+        <h2 dangerouslySetInnerHTML={createMarkup(card.title)} />
+        {card.media && card.media.length ? (
           renderMediaPreview(card.media)
         ) : (
           <div>No Media Provided</div>
         )}
-        <div dangerouslySetInnerHTML={createMarkup(card.description)}></div>
+        <div dangerouslySetInnerHTML={createMarkup(card.description)} />
       </Modal>
     </>
   );
